9d8a5e0bc8a19044968c48c4d555fdee724f282c
pooneh <poonehshooshtari@gmail.com>
2018-05-29 15:16:55 -0600

    Refactored the code (javascript file)

--- static/script.js
+++ static/script.js
@@ -6,21 +6,20 @@ document.addEventListener("DOMContentLoaded", function(event){
 		if(navigator.geolocation){
 			navigator.geolocation.getCurrentPosition(showPosition);
 		}else{
-			console.log("Geolocation is not supported by this browser.");
+			console.error("Geolocation is not supported by this browser.");
 		}
 	}
 
 	function showPosition(position){
-		let latitude = position.coords.latitude;
-		let longitude = position.coords.longitude;
+		let {latitude, longitude} = position.coords;
 		let localWeatherRequest = new XMLHttpRequest();
-		localWeatherRequest.open("GET", 'https://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+
+		localWeatherRequest.open("GET", 'https://api.openweathermap.org/data/2.5/weather?lat='+position.coords.latitude+'&lon='+position.coords.longitude+
 				'&APPID=9492469ab458cfd72f7c962d1d6512ed');
 
 		localWeatherRequest.onload = function(){
 				localWeather = JSON.parse(localWeatherRequest.responseText);
 				let iconCode = localWeather.weather[0].icon;
-				let iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+				let iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
 				document.querySelector(".WeatherIcon").innerHTML = "<img src='"+iconUrl+"'>";
 				document.querySelector(".tempC").innerHTML = (Math.round(Number(localWeather.main.temp) - 273.15)+"C");
 				
@@ -29,14 +28,16 @@ document.addEventListener("DOMContentLoaded", function(event){
 	}
 
 	document.querySelector(".convertTempUnit").addEventListener("click", function(){
-		if(celsius){
-			document.querySelector(".tempC").innerHTML = (Math.round(Number(localWeather.main.temp) - 273.15)*9/5+32+"F");
-			document.querySelector(".convertTempUnit").value = "Convert to C";
-			celsius = false;
-		}else{
-			document.querySelector(".tempC").innerHTML = (Math.round(Number(localWeather.main.temp) - 273.15)+"C");
-			document.querySelector(".convertTempUnit").value = "Convert to F";
-			celsius = true;
+		if(localWeather){
+			if(celsius){
+				document.querySelector(".tempC").innerHTML = (Math.round((Number(localWeather.main.temp) - 273.15)*9/5+32)+"F");
+				document.querySelector(".convertTempUnit").value = "Convert to C";
+				celsius = false;
+			}else{
+				document.querySelector(".tempC").innerHTML = (Math.round(Number(localWeather.main.temp) - 273.15)+"C");
+				document.querySelector(".convertTempUnit").value = "Convert to F";
+				celsius = true;
+			}
 		}	
 	});
 
